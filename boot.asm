50794792521a5e129bf7edd9537bf0f71bf49eb5
Pyroger <KudrinKonstantinA@yandex.ru>
2025-10-19 20:40:25 +0300

    Добавление загрузки ядра через lba в boot.asm Добавление kernel.asm

--- boot.asm
+++ boot.asm
@@ -9,9 +9,61 @@ start:
     mov ss, ax ;установка начала стека на адрес 0x0000
     mov sp, 0x7C00 ;установка конца стека на адрес 0x7C00
 
-    mov si, msg
+    mov [boot_drive], dl ;сохраняем номер диска
+
+    mov si, msg_loaded
     call print_string
 
+    mov si, kernel_read
+    mov dl, [boot_drive]
+    mov ah, 0x42 ;указывает что надо использовать lba, а не chs
+    int 0x13 ;прерывание BIOS для работы с диском
+    jc disk_error
+
+    jmp 0x0000:0x1000 ;переходим на ядро
+
+disk_error:
+    mov si, msg_disk_error
+    call print_string
+
+    mov al, ah 
+    call print_hex
+
+    jmp $ ;бесконечный цикл
+
+
+print_hex:
+    mov bl, al ;сохраняем al
+    
+    ; Выводим "0x"
+    mov si, hex_prefix
+    call print_string
+    
+    ; Верхняя тетрада
+    mov al, bl
+    mov cl, 4
+    shr al, cl 
+    call .print_digit
+    
+    ; Нижняя тетрада  
+    mov al, bl
+    and al, 0x0F
+    call .print_digit
+
+    ret
+    
+.print_digit:
+    cmp al, 9
+    jbe .digit
+    add al, 7
+.digit:
+    add al, 0x30
+    mov ah, 0x0E
+    int 0x10
+    ret
+
+
+;Функция печати текста
 print_string:
     mov ah, 0x0E ;указывает на функцию телетайпного вывода символа (по простому - печатает один символ на экран)
 .loop:
@@ -23,6 +75,20 @@ print_string:
 .done:
     ret
 
-msg: db "Boot loaded" 
+
+;Область данных
+msg_loaded: db "Boot loaded", 13, 10, 0
+msg_disk_error: db "Disk error: ", 0
+boot_drive: db 0
+hex_prefix: db "0x", 0
+
+kernel_read: ;DAPS
+    db 0x10 ;размер пакета (16 байт)
+    db 0
+    dw 2 ;врядли вывод строки kernel loaded будет веслить больше 1кб так что с запасиком
+    dw 0x1000 ;через [org 0x1000] укажем в коде ядра
+    dw 0x0000
+    dq 1 ;отсчет с 0, а первые 512 байт занимает загрузчик
+
 times 510 - ($ - $$) db 0 ;заполняет остаток файла до 512 байт нулями (не знаю почему такая странная формула вычисления, но работает)
 dw 0xAA55 ; Сигнатура загрузочного сектора (чтоб BIOS нашел)
--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 nasm -f bin boot.asm -o boot.bin
 
+nasm -f bin kernel.asm -o kernel.bin
+
 dd if=/dev/zero of=myos.hdd bs=512 count=20480>/dev/null
 
 dd if=boot.bin of=myos.hdd bs=512 count=1 conv=notrunc 2>/dev/null
+
+dd if=kernel.bin of=myos.hdd bs=512 seek=1 conv=notrunc 2>/dev/null
+read
+
 
